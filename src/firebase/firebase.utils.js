0f7d040cd528c412013cb2f5a10ce5fe968a8f8d
Taylor Beck <beck.taylorg@gmail.com>
2021-03-21 12:28:27 -0700

    Fix dependency issues and firebase configuration

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyA2bV2OwTOzkNfcyWc1EBlJuabvgKFiUDg',
+  authDomain: 'clothing-store-db-208c2.firebaseapp.com',
+  projectId: 'clothing-store-db-208c2',
+  storageBucket: 'clothing-store-db-208c2.appspot.com',
+  messagingSenderId: '368359101591',
+  appId: '1:368359101591:web:2eeb76b54689ec84bf45d2',
+  measurementId: 'G-QPGC087VBP'
 };
 
 firebase.initializeApp(config);
